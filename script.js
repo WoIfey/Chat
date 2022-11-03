e55141a7551816c1f6cf45a9fc3ca1afcb4afa53
WoIfey <wuflerz@outlook.com>
2022-11-03 22:58:48 +0200

    message fix

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function initDatabase() {
     const message = document.createElement("li");
 
     message.innerText =
-      new Date(data.key).toLocaleDateString("fi-FI") + ": " + data.val();
+      new Date(data.key).toLocaleString("fi-FI") + ": " + data.val();
 
     list.appendChild(message);
   });
